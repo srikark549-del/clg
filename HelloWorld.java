049cf0a116c7f28d4ccd58e1214c104099bdbdc8
Srikar <srikark549@gmail.com>
2026-09-15 16:02:24 +0530

    Centered message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 public class HelloWorld {
     public static void main(String[] args){
           JFrame frame = new JFrame();
-          JLabel label = new JLabel("Hello World");
+          JLabel label = new JLabel("Hello World",SwingConstants.CENTER);
           frame.setVisible(true);
           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
           frame.setSize(400,400);
