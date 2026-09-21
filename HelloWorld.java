f59d9e80209ad40ccc416d86897d90bd34fd53aa
Srikar <srikark549@gmail.com>
2026-09-21 12:05:17 +0530

    Add HelloWorld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,43 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-public class HelloWorld {
-    public static void main(String[] args){
-          JFrame frame = new JFrame();
-          JLabel label = new JLabel("Hello World",SwingConstants.CENTER);
-          frame.setVisible(true);
-          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          frame.setSize(400,400);
-          frame.add(label);
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class HelloWorld implements ActionListener {
+    static JTextField x2;
+    static JTextField y2;
+    static JTextField resField;
+    public static void main(String args[]) {
+        JFrame frame = new JFrame();
+        FlowLayout fw = new FlowLayout();
+        frame.setLayout(fw);
+        frame.setSize(750, 350);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel x1 = new JLabel("Number 1:");
+        frame.add(x1);
+        x2 = new JTextField(15);
+        frame.add(x2);
+        JLabel y1 = new JLabel("Number 2:");
+        frame.add(y1);
+        y2 = new JTextField(15);
+        frame.add(y2);
+        JLabel res = new JLabel("Result:");
+        frame.add(res);
+        resField = new JTextField(20);
+        frame.add(resField);
+        JButton a = new JButton("DIVIDE");
+        frame.add(a);
+        HelloWorld obj = new HelloWorld();
+        a.addActionListener(obj);
+        frame.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int a = Integer.parseInt(x2.getText());
+        int b = Integer.parseInt(y2.getText());
+        float c = (float) a / b;
+        resField.setText(c + "");
     }
 }
